6e3fc311b255d898dcdc30b4d75704707c4d8594
Nasser Jamal <nasserjamal30@gmail.com>
2023-01-16 15:55:13 +0300

    l

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,14 +16,13 @@ API = "https://jsonplaceholder.typicode.com"
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
-            resp_user = requests.get(f'{API}/users?id={sys.argv[1]}')
-            resp_todo = requests.get(f'{API}/todos?userId={sys.argv[1]}')
-            if resp_user.status_code != 200 or resp_todo.status_code != 200:
-                exit()
-            user_info = resp_user.json()
-            todos_completed = [i for i in resp_todo.json() if i.get('completed')]
-            todos_all = resp_todo.json()
+            id = int(sys.argv[1])
+            resp_user = requests.get('{}/users/{}'.format(API, id)).json()
+            resp_todo = requests.get('{}/todos'.format(API)).json()
+            user_info = resp_user
+            todos_completed = [i for i in resp_todo if i.get('completed')]
+            todos_all = resp_todo
             c = len(todos_completed)
             all = len(todos_all)
-            print(f'Employee {user_info[0].get("name")} is done with tasks({c}/{all}):')
-            [print(f'\t {todo.get("title")}') for todo in todos_completed]
+            print(f"Employee {user_info[0].get('name')} is done with tasks({c}/{all}):")
+            [print(f"\t {todo.get('title')}") for todo in todos_completed]
